ce49565a60f34ee151c5b48c72e85e29b341edc3
aaiden Nicolas Witten <aaidenplays@yahoo.com>
2019-12-28 22:21:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -26,8 +26,8 @@ def retrieve_element_from_index(array, index_number)
   
 end
 
-def retrieve_first_element_from_array(array, index)
-  array[index]
+def retrieve_first_element_from_array(array)
+  
 end
 
 def retrieve_last_element_from_array(array)
